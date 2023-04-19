ac5322be907d50ac7fb8d6ee12f3250a8e2aae35
ssshun3 <s.kudo.22.7.584@4nonome.com>
2023-04-19 16:53:19 +0900

    環境構築、styled-component,hook,recoil

---
+++ src/components/atoms/title/index.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta } from "@storybook/react";
+import { Title } from ".";
+
+export default {
+  title: "Atoms/Title",
+  component: Title,
+} as Meta;
+
+export function Default() {
+  return <Title />;
+}
---
+++ src/components/atoms/title/style.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledTitle = styled.div`
+  color: #9b06b3;
+  font-size: 48px;
+  font-family: sans-serif;
+`;
